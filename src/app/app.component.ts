5269ff882b513f2e916bd6715a80ccfc1dbee162
safwan <safwan.mah99@gmail.com>
2020-08-30 23:13:12 -0700

    Angularpdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import * as jsPDF from 'jspdf';
+
+import jsdPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pdf';
+
+  // constructor(){
+  //   this.downloadPDF();
+  // }
+
+  downloadPDF() {
+    console.log(jsdPDF);
+    const doc = new jsdPDF();
+    console.log(doc);
+    doc.text('Welcome To Carwow \n This is a bloody invoice \n Best Car Buying Site***', 10, 10);
+    doc.save('a4.pdf');
+  }
+
+
 }
